c0fe0b7cddb8b67de29b16ed55f1734a10f87986
Gyaanendra <74192896+Gyaanendra@users.noreply.github.com>
2021-07-10 09:51:27 +0530

    Add files via upload

---
+++ calculation.py
@@ -0,0 +1,13 @@
+# programe too perfrom basic calculation
+a = int(input("Enter num1:"))
+b = int(input("Enter num2:"))
+# calculation
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+
+print("Addition:",c)
+print("Subtraction:",d)
+print("Multipy:",e)
+print("divide:",f)
