f8809e1189f0df7d18be5ccb227e5ff00f2bda2b
Gyaanendra <74192896+Gyaanendra@users.noreply.github.com>
2021-08-13 08:09:10 +0530

    Update Fibonacci series..py

--- Fibonacci series..py
+++ Fibonacci series..py
@@ -1,11 +1,11 @@
 k = int(input("enter the num :"))
 a = 0
-b = 0
-c = 1
+b = 1
 print(" here is fibnnoic sereis up to",k)
-while a <=k:
-    print(a)
+print(a)
+for c in range(k):
     c = b+a
     a=b
     b=c
-    a+=1      
+    print(a)    
+
