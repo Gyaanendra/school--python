7553c6b4436ad866ab2a45cc89dfb796c9604398
Gyaanendra <74192896+Gyaanendra@users.noreply.github.com>
2021-08-17 20:58:44 +0530

    Update plaindrom.py

--- plaindrom.py
+++ plaindrom.py
@@ -1,7 +1,11 @@
+#inputing a string from user 
 s = input("Enter the word:")
+#reversing it 
 rs = (s[::-1])
 
-if rs == s:
+#checking if they are equal
+if s == rs:
     print('it is a palindrome')
+#if not 
 else:
     print('it is not a palindrome')
